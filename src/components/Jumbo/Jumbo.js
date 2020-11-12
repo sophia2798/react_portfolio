6ae2b84db8b7c4d0c6d3ab87be54253bce831f9f
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-11-11 18:04:18 -0800

    added jumbo styling

--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -9,7 +9,7 @@ function Jumbo() {
     return (
         <div className="jumbo">
             <div className="jumbotron jumbotron-fluid">
-            <div className="set">
+                {/* <div className="set jumbo-set">
                 <div><img src={img1} style={{height:"5rem"}}/></div>
                 <div><img src={img2} style={{height:"5rem"}}/></div>
                 <div><img src={img3} style={{height:"5rem"}}/></div>
@@ -20,7 +20,7 @@ function Jumbo() {
                 <div><img src={img2} style={{height:"5rem"}}/></div>
                 <div><img src={img3} style={{height:"5rem"}}/></div>
             </div>
-            <div className="set set2">
+            <div className="set set2 jumbo-set">
                 <div><img src={img1} style={{height:"5rem"}}/></div>
                 <div><img src={img2} style={{height:"5rem"}}/></div>
                 <div><img src={img3} style={{height:"5rem"}}/></div>
@@ -31,7 +31,7 @@ function Jumbo() {
                 <div><img src={img2} style={{height:"5rem"}}/></div>
                 <div><img src={img1} style={{height:"5rem"}}/></div>
             </div>
-            <div className="set set3">
+            <div className="set set3 jumbo-set">
                 <div><img src={img2} style={{height:"5rem"}}/></div>
                 <div><img src={img2} style={{height:"5rem"}}/></div>
                 <div><img src={img3} style={{height:"5rem"}}/></div>
@@ -41,24 +41,160 @@ function Jumbo() {
                 <div><img src={img3} style={{height:"5rem"}}/></div>
                 <div><img src={img2} style={{height:"5rem"}}/></div>
                 <div><img src={img3} style={{height:"5rem"}}/></div>
+            </div> */}
+                <div className="jumbo-content row">
+                    <div className="col-6 grid-col">
+                        <ul id="hexGrid">
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTUKzK4DgFftnLNJE5N8DkGfeCRq8_amqbSYg&usqp=CAU" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://media.istockphoto.com/photos/black-and-white-colourclose-up-fingers-typing-on-the-computer-picture-id1047951828?k=6&m=1047951828&s=170667a&w=0&h=9SKefXT1mVnxjKYRBRsI4vyYhKmWOmAl_n_OGdC38Ko=" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://images.fineartamerica.com/images-medium-large/black-and-white-sky-with-building-storm-clouds-photo-poster-print-keith-webber-jr.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/cherry-blossoms-black-and-white-1-paul-ward.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://ak.picdn.net/shutterstock/videos/7138747/thumb/1.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://images.fineartamerica.com/images-medium-large/black-and-white-sky-with-building-storm-clouds-photo-poster-print-keith-webber-jr.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://media.istockphoto.com/photos/black-and-white-colourclose-up-fingers-typing-on-the-computer-picture-id1047951828?k=6&m=1047951828&s=170667a&w=0&h=9SKefXT1mVnxjKYRBRsI4vyYhKmWOmAl_n_OGdC38Ko=" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://evolllution.com/wp-content/uploads/2013/12/Big-T-Technology-chones.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://evolllution.com/wp-content/uploads/2013/12/Big-T-Technology-chones.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://ak.picdn.net/shutterstock/videos/7138747/thumb/1.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://images.fineartamerica.com/images-medium-large/black-and-white-sky-with-building-storm-clouds-photo-poster-print-keith-webber-jr.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTUKzK4DgFftnLNJE5N8DkGfeCRq8_amqbSYg&usqp=CAU" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://ak.picdn.net/shutterstock/videos/6433316/thumb/4.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://media.istockphoto.com/photos/black-and-white-colourclose-up-fingers-typing-on-the-computer-picture-id1047951828?k=6&m=1047951828&s=170667a&w=0&h=9SKefXT1mVnxjKYRBRsI4vyYhKmWOmAl_n_OGdC38Ko=" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://images.fineartamerica.com/images-medium-large/black-and-white-sky-with-building-storm-clouds-photo-poster-print-keith-webber-jr.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/cherry-blossoms-black-and-white-1-paul-ward.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://ak.picdn.net/shutterstock/videos/6433316/thumb/4.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                            <li class="hex">
+                                <a class="hexIn" href="#">
+                                    <img src="https://images.fineartamerica.com/images-medium-large/black-and-white-sky-with-building-storm-clouds-photo-poster-print-keith-webber-jr.jpg" alt="" />
+                                    <h1>This is a title</h1>
+                                    <p>Some sample text about the article this hexagon leads to</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-6">
+                        <h2>Hello</h2>
+                    </div>
+                </div>
             </div>
-            <div className="jumbo-content">
-                <section className="jumbo-col" style={{position:'absolute',left:0}}>
-                    <FilterDramaIcon/><strong>&nbsp;dreamer&nbsp;</strong> <FilterDramaIcon/>
-                    <p>Every great progression, movement, or product begins with a dream. As the world becomes more interconnected, technology becomes more powerful, and tools become more accessible, this generation inches closer and closer to turning its dreams into a reality. While I can be an implementer, I strive to continue to open my mind to the possibilities of the future.</p>
-                </section>
-                <section className="jumbo-col2" style={{position:'absolute',left:'30%'}}>
-                I AM A . . .
-                </section>
-                <section className="jumbo-col" style={{position:'absolute',right:0}}>
-                    <strong>&lt;developer&gt;</strong>
-                    <p>In the modern age, the ability to express ideas and products through websites or mobile applications is crucial. Web development requires a blend of technical and creative skill while also being an everchanging field. The constant innovation always pushes me to always be learning and approach problems in new ways.</p>
-                </section>
-            </div>
-            </div>
-            <div className="blur"></div>
+            {/* <div className="blur"></div> */}
         </div>
     )
 }
 
 export default Jumbo
+
+
+{/* <section className="jumbo-col" style={{position:'absolute',left:0}}>
+<FilterDramaIcon/><strong>&nbsp;dreamer&nbsp;</strong> <FilterDramaIcon/>
+<p>Every great progression, movement, or product begins with a dream. As the world becomes more interconnected, technology becomes more powerful, and tools become more accessible, this generation inches closer and closer to turning its dreams into a reality. While I can be an implementer, I strive to continue to open my mind to the possibilities of the future.</p>
+</section>
+<section className="jumbo-col2" style={{position:'absolute',left:'30%'}}>
+<p><small>HELLO</small></p>
+<p>I'M SOPHIA JUNG</p>
+</section>
+<section className="jumbo-col" style={{position:'absolute',right:0}}>
+<strong>&lt;developer&gt;</strong>
+<p>In the modern age, the ability to express one's thoughts, ideas, or products though technological platforms has become crucial. I am passionate about helping people and organizations pursue such an endeavor. I particularly enjoy the blend of technical and creative skill that is required, as well as the dyanmic aspect of the field, which always pushes me to learn new things and approach problems in new ways.</p>
+</section> */}
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,13 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { HashLink } from 'react-router-hash-link';
-import logo from "../../assets/logo2.png";
+// import logo from "../../assets/logo2.png";
 
 function Navbar() {
     return (
         <div className='Navbar' style={{position:'relative', zIndex:6}}>
             <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor:'#171717'}}>
-            <Link className="navbar-brand" to="/main"><img src={logo} alt="logo" /></Link>
+            <Link className="navbar-brand" to="/main">SOPHIA JUNG</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
