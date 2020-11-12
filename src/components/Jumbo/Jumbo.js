6b6ee4689f90c822a4efe80432f87e9969cb9c77
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-11-11 20:36:29 -0800

    added hex grid

--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -46,134 +46,134 @@ function Jumbo() {
                     <div className="col-6 grid-col">
                         <ul id="hexGrid">
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTUKzK4DgFftnLNJE5N8DkGfeCRq8_amqbSYg&usqp=CAU" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://media.istockphoto.com/photos/black-and-white-colourclose-up-fingers-typing-on-the-computer-picture-id1047951828?k=6&m=1047951828&s=170667a&w=0&h=9SKefXT1mVnxjKYRBRsI4vyYhKmWOmAl_n_OGdC38Ko=" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://images.fineartamerica.com/images-medium-large/black-and-white-sky-with-building-storm-clouds-photo-poster-print-keith-webber-jr.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/cherry-blossoms-black-and-white-1-paul-ward.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://ak.picdn.net/shutterstock/videos/7138747/thumb/1.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://images.fineartamerica.com/images-medium-large/black-and-white-sky-with-building-storm-clouds-photo-poster-print-keith-webber-jr.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://media.istockphoto.com/photos/black-and-white-colourclose-up-fingers-typing-on-the-computer-picture-id1047951828?k=6&m=1047951828&s=170667a&w=0&h=9SKefXT1mVnxjKYRBRsI4vyYhKmWOmAl_n_OGdC38Ko=" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://evolllution.com/wp-content/uploads/2013/12/Big-T-Technology-chones.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://evolllution.com/wp-content/uploads/2013/12/Big-T-Technology-chones.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://ak.picdn.net/shutterstock/videos/7138747/thumb/1.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://images.fineartamerica.com/images-medium-large/black-and-white-sky-with-building-storm-clouds-photo-poster-print-keith-webber-jr.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTUKzK4DgFftnLNJE5N8DkGfeCRq8_amqbSYg&usqp=CAU" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://ak.picdn.net/shutterstock/videos/6433316/thumb/4.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://media.istockphoto.com/photos/black-and-white-colourclose-up-fingers-typing-on-the-computer-picture-id1047951828?k=6&m=1047951828&s=170667a&w=0&h=9SKefXT1mVnxjKYRBRsI4vyYhKmWOmAl_n_OGdC38Ko=" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://images.fineartamerica.com/images-medium-large/black-and-white-sky-with-building-storm-clouds-photo-poster-print-keith-webber-jr.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/cherry-blossoms-black-and-white-1-paul-ward.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://ak.picdn.net/shutterstock/videos/6433316/thumb/4.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                             <li class="hex">
-                                <a class="hexIn" href="#">
+                                <div class="hexIn" href="#">
                                     <img src="https://images.fineartamerica.com/images-medium-large/black-and-white-sky-with-building-storm-clouds-photo-poster-print-keith-webber-jr.jpg" alt="" />
                                     <h1>This is a title</h1>
                                     <p>Some sample text about the article this hexagon leads to</p>
-                                </a>
+                                </div>
                             </li>
                         </ul>
                     </div>
-                    <div className="col-6">
+                    <div className="col-6 header-text">
                         <h2>Hello</h2>
                     </div>
                 </div>
